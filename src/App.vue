1e834b79c0ec1eaed74c5d812419da6867532688
Almaz Khabibullin <adreams2016@yandex.ru>
2025-08-05 20:25:35 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+let item = 1;
+let classRed = "red";
+</script>
+
+<template>
+  <h1>Привет</h1>
+  <main :class="`color-${classRed}`">Контент</main>
+</template>
+
+<style scoped>
+
+</style>
