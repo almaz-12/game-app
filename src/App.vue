7d7e9281459ec7598be29da1de7f0ac369ced639
Almaz Khabibullin <adreams2016@yandex.ru>
2025-08-14 18:56:33 +0300

    создание компонента кнопки

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup>
-
+import BaseButton from './components/BaseButton.vue';
 </script>
 
 <template>
+  <BaseButton>
+    Начать игру
+  </BaseButton>
   <p>{{ new Date().toLocaleString() }}</p>
 </template>
 
