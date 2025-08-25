7d08ad5c5a3f07c4617e7e635d0d01d8a72cd5ff
Almaz Khabibullin <adreams2016@yandex.ru>
2025-08-25 18:14:20 +0300

    состояния карточек

--- src/App.vue
+++ src/App.vue
@@ -5,25 +5,63 @@ import CardItem from './components/CardItem.vue';
 import { ref } from 'vue';
 
 const gameScore = ref(0);
-const data = ref({
+const data = ref([{
+  id: 0,
   word: 'hello',
   translation: 'привет',
   state: 'closed',
   status: 'pending',
-});
+},{
+  id: 1,
+  word: 'apple',
+  translation: 'яблоко',
+  state: 'closed',
+  status: 'pending',
+},{
+  id: 2,
+  word: 'book',
+  translation: 'книга',
+  state: 'closed',
+  status: 'pending',
+},{
+  id: 3,
+  word: 'water',
+  translation: 'вода',
+  state: 'closed',
+  status: 'pending',
+}]);
 
 const isStart = ref(false);
 
 function startGame() {
   isStart.value = true;
+  gameScore.value = 0;
 }
-function handleTrun(data) {
-  console.log(data);
+function handleTrun(id) {
+   console.log(id);
+  const item = data.value.find(item => item.id === id);
+  console.log(item);
+  if(item) {
+    item.state = 'opened';
+  }
 }
-function handleGetResult(data) {
-  console.log(data);
+function handleSuccessResult(id) {
   if(gameScore.value < 0) return;
-  (data === 'success') ? gameScore.value++ : gameScore.value--
+  gameScore.value++
+
+  const item = data.value.find(item => item.id === id);
+  if(item) {
+    item.status = 'success';
+  }
+}
+function handleFailedResult(id) {
+  if(gameScore.value < 0) return;
+  gameScore.value--
+
+  const item = data.value.find(item => item.id === id);
+  if(item) {
+    item.status = 'failed';
+  }
 }
 </script>
 
@@ -33,7 +71,16 @@ function handleGetResult(data) {
     <BaseButton v-if="!isStart" @click="startGame">
       Начать игру
     </BaseButton>
-    <CardItem v-else :data="data" @show-answer="handleTrun" @process-result="handleGetResult"/>
+    <div class="game-box" v-else>
+      <CardItem
+      v-for="item in data"
+      :key="item.id"
+      :data="item"
+      @open-card="handleTrun"
+      @success-action="handleSuccessResult"
+      @failed-action="handleFailedResult"
+    />
+    </div>
   </main>
 </template>
 
@@ -42,5 +89,15 @@ function handleGetResult(data) {
   display: grid;
   place-items: center;
   min-height: calc(100vh - 125px);
+  max-width: 1200px;
+  margin: auto;
+  padding: 0 15px;
+}
+
+.game-box {
+  display: grid;
+  gap: 20px;
+  grid-template-columns: repeat(4, 1fr);
+  width: 100%;
 }
 </style>
