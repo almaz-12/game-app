7ba34e95afb58037a765398be9a5dd6a21b05e56
Almaz Khabibullin <adreams2016@yandex.ru>
2025-08-28 14:40:24 +0300

    запрос к api

--- src/App.vue
+++ src/App.vue
@@ -2,45 +2,21 @@
 import AppHeader from './layouts/AppHeader.vue';
 import BaseButton from './components/BaseButton.vue';
 import CardItem from './components/CardItem.vue';
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 
 const gameScore = ref(0);
-const data = ref([{
-  id: 0,
-  word: 'hello',
-  translation: 'привет',
-  state: 'closed',
-  status: 'pending',
-},{
-  id: 1,
-  word: 'apple',
-  translation: 'яблоко',
-  state: 'closed',
-  status: 'pending',
-},{
-  id: 2,
-  word: 'book',
-  translation: 'книга',
-  state: 'closed',
-  status: 'pending',
-},{
-  id: 3,
-  word: 'water',
-  translation: 'вода',
-  state: 'closed',
-  status: 'pending',
-}]);
+const worldList = ref([]);
 
 const isStart = ref(false);
+const errorMessage = ref(null);
+const isLoading = ref(false);
 
 function startGame() {
   isStart.value = true;
   gameScore.value = 0;
 }
 function handleTrun(id) {
-   console.log(id);
-  const item = data.value.find(item => item.id === id);
-  console.log(item);
+  const item = worldList.value.find(item => item.id === id);
   if(item) {
     item.state = 'opened';
   }
@@ -49,7 +25,7 @@ function handleSuccessResult(id) {
   if(gameScore.value < 0) return;
   gameScore.value++
 
-  const item = data.value.find(item => item.id === id);
+  const item = worldList.value.find(item => item.id === id);
   if(item) {
     item.status = 'success';
   }
@@ -58,22 +34,54 @@ function handleFailedResult(id) {
   if(gameScore.value < 0) return;
   gameScore.value--
 
-  const item = data.value.find(item => item.id === id);
+  const item = worldList.value.find(item => item.id === id);
   if(item) {
     item.status = 'failed';
   }
 }
+
+onMounted(async () => {
+  try {
+    errorMessage.value = null;
+    isLoading.value = true;
+    const response = await fetch('http://localhost:8080/api/random-words');
+
+    if(!response.ok) throw new Error(`Ошибка HTTP: ${response.status}`)
+
+    const data = await response.json();
+
+    if(data.length) {
+      data.map((item,index) => {
+        worldList.value.push({
+          ...item,
+          id: index,
+          state: 'closed',
+          status: 'pending',
+        })
+      })
+    }
+
+    return data;
+  } catch (error) {
+    errorMessage.value = error;
+    throw error;
+  } finally {
+    isLoading.value = false
+  }
+})
 </script>
 
 <template>
   <AppHeader :score="gameScore"/>
   <main class="main">
-    <BaseButton v-if="!isStart" @click="startGame">
+    <div v-if="isLoading">Загрузка игры...</div>
+    <div v-else-if="errorMessage">{{ errorMessage }}</div>
+    <BaseButton v-else-if="!isStart" @click="startGame">
       Начать игру
     </BaseButton>
     <div class="game-box" v-else>
       <CardItem
-      v-for="item in data"
+      v-for="item in worldList"
       :key="item.id"
       :data="item"
       @open-card="handleTrun"
@@ -91,7 +99,7 @@ function handleFailedResult(id) {
   min-height: calc(100vh - 125px);
   max-width: 1200px;
   margin: auto;
-  padding: 0 15px;
+  padding: 0 15px 50px;
 }
 
 .game-box {
