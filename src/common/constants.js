072b2e37b41cea502d6d455ec2af2e2839fa64f7
Almaz Khabibullin <adreams2016@yandex.ru>
2025-09-03 16:19:06 +0300

    кнопка начать заново, рефакторинг

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,3 +1,6 @@
 export const API_ENDPOINT = 'http://localhost:8080/api';
 export const STATE_START = 'closed';
 export const STATUS_START = 'pending';
+
+export const SCORE_SUCCESS = 10;
+export const SCORE_FAIL = 4;
